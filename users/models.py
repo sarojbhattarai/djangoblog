21524d14d3012427a98b1ff3fd3f4d45bc59e673
Saroj Bhattarai <sarojbhattarai800@gmail.com>
2018-12-28 21:14:26 +0545

    User pofile update and photo of blogger on blog title

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image 
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,5 +9,21 @@ class Profile(models.Model):
 
 	def __str__(self):
 		return f'{self.user.username} Profile'
+
+		### resizing the image to 300 px
+	def save(self):
+		super().save()
+
+		img = Image.open(self.image.path)
+
+		if img.height > 300 or img.width > 300:
+			output_size = (300,300)
+			img.thumbnail(output_size)
+			img.save(self.image.path)
+
+
+
+
+
 
 
